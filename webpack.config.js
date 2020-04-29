4040fed15a2d77858a4f08ae702ac2285101bd6f
yuenaka <yuki.u.1224@gmail.com>
2020-04-29 21:35:31 +0900

    webpack設定

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require("path");
+const VueLoaderPlugin = require("vue-loader/lib/plugin");
+
+module.exports = [
+  {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+      filename: "bundle.js",
+      path: path.join(__dirname, "./dist/")
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: "vue-loader"
+        }
+      ]
+    },
+    plugins: [new VueLoaderPlugin()]
+  }
+];
