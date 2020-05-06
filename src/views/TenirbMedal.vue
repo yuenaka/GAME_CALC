d2f642be8e8e0f525c22af9fe739f578acc43402
yuenaka <yuki.u.1224@gmail.com>
2020-05-06 09:33:11 +0900

    modify: 文言修正

--- src/views/TenirbMedal.vue
+++ src/views/TenirbMedal.vue
@@ -3,7 +3,9 @@
     <div class="note">※ 入力フォームのフォーカスを外すと計算処理が走ります</div>
     <div class="box box-goal_setting">
       <div class="row">
-        <div class="item-title"><label>目標ポイント</label></div>
+        <div class="item-title">
+          <label>目標ポイント</label>
+        </div>
         <div class="item-text">
           <div class="input">
             <input type="number" v-model.number="goalPt" v-on:blur="calc" />
@@ -12,7 +14,9 @@
         </div>
       </div>
       <div class="row">
-        <div class="item-title"><label>現在のポイント</label></div>
+        <div class="item-title">
+          <label>現在のポイント</label>
+        </div>
         <div class="item-text">
           <div class="input">
             <input type="number" v-model.number="initialPt" v-on:blur="calc" />
@@ -26,17 +30,15 @@
         </div>
         <div class="item-text">
           <div class="input">
-            <input
-              type="number"
-              v-model.number="initialMedal"
-              v-on:blur="calc"
-            />
+            <input type="number" v-model.number="initialMedal" v-on:blur="calc" />
           </div>
           <span class="unit">枚</span>
         </div>
       </div>
       <div class="row">
-        <div class="item-title"><label>イベント残り日数</label></div>
+        <div class="item-title">
+          <label>イベント残り日数</label>
+        </div>
         <div class="item-text">
           <div class="input">
             <input type="number" v-model.number="period" v-on:blur="calc" />
@@ -82,16 +84,14 @@
       </div>
       <div class="row">
         <div class="item-title">
-          <label> ―ポイボSRの特攻ボーナスを加算する<sup>※1</sup></label>
+          <label>
+            ―ポイボSRの特攻ボーナスを加算する
+            <sup>※1</sup>
+          </label>
         </div>
         <div class="item-text">
           <div class="input">
-            <input
-              id="isUseSR"
-              type="checkbox"
-              v-model="isUseSR"
-              v-on:change="calc"
-            />
+            <input id="isUseSR" type="checkbox" v-model="isUseSR" v-on:change="calc" />
             <label for="isUseSR" class="checkbox">&thinsp;</label>
           </div>
         </div>
@@ -103,12 +103,7 @@
         <div class="item-text">
           <div class="input">
             <select v-model="nomalLevel" v-on:blur="calc">
-              <option
-                v-for="(name, value) in nomalLevelList"
-                :key="value"
-                :value="value"
-                >{{ name }}</option
-              >
+              <option v-for="(name, value) in nomalLevelList" :key="value" :value="value">{{ name }}</option>
             </select>
           </div>
         </div>
@@ -120,12 +115,7 @@
         <div class="item-text">
           <div class="input">
             <select v-model="eventLevel" v-on:blur="calc">
-              <option
-                v-for="(name, value) in eventLevelList"
-                :key="value"
-                :value="value"
-                >{{ name }}</option
-              >
+              <option v-for="(name, value) in eventLevelList" :key="value" :value="value">{{ name }}</option>
             </select>
           </div>
         </div>
@@ -133,19 +123,26 @@
     </div>
     <div class="note">
       ※1)
-      チェックが入っている場合、累計ポイントがポイボSR取得ラインに到達した時点からポイボSRの特攻ボーナスを加算します。<br />
-      ポイボSRをオーダーに組み込まない場合はチェックを外してください。<br />
-      チェックを入れた場合、「特攻ボーナス」にはポイボSR分を除いた数値を入力してください。
+      チェックが入っている場合、累計ポイントがポイボSR取得ラインに到達した時点からポイボSRの特攻ボーナスを加算します。
+      <br />ポイボSRをオーダーに組み込まない場合はチェックを外してください。
+      <br />チェックを入れた場合、「特攻ボーナス」にはポイボSR分を除いた数値を入力してください。
     </div>
 
     <div class="box box-result">
       <p>
-        {{ period }}日で{{ goalPt }}Ptを達成するには、<br />
-        1日あたり{{ (goalPt / period).toFixed(1) }}Pt稼ぐ必要があります。<br />
-        通常曲を {{ Math.ceil(nomalPlayTimes / period) }} 回、<br />
-        イベント曲を
-        {{ Math.ceil(eventPlayTimes / period) }} 回ほど<br />
-        プレイしましょう。<sup>※2</sup><br />
+        {{ period }}日で{{ goalPt }}Ptを達成するには、
+        <br />
+        1日あたり{{
+        ((goalPt - initialPt) / period).toFixed(1)
+        }}Pt稼ぐ必要があります。
+        <br />これはおよそ
+        <br />
+        通常曲 {{ Math.ceil(nomalPlayTimes / period) }} 回 +
+        イベント曲
+        {{ Math.ceil(eventPlayTimes / period) }} 回
+        <br />のプレイに相当します。
+        <sup>※2</sup>
+        <br />
       </p>
     </div>
     <div class="note">※2) ブーストを使わない場合</div>
